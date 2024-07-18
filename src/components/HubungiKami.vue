1d1a845fa063010a2d009aec378d75299e396b76
Yuda Aradea <yudaaradeagit@gmail.com>
2024-07-18 13:33:26 +0700

    w-full

--- src/components/HubungiKami.vue
+++ src/components/HubungiKami.vue
@@ -25,7 +25,7 @@ import Title from "./Title.vue";
       <div>
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15860.95303000204!2d106.24494983476482!3d-6.363203022379457!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e42112b82f4aa29%3A0x2a9c4cf2496990d5!2sKp.%20Jaura%2C%20West%20Rangkasbitung%2C%20Rangkasbitung%2C%20Lebak%20Regency%2C%20Banten!5e0!3m2!1sen!2sid!4v1721278940203!5m2!1sen!2sid"
-          class="rounded-xl w-[350px] lg:w-[600px] md:w-full h-[350px] lg:h-[450px]"
+          class="rounded-xl lg:w-[600px] w-full h-[350px] lg:h-[450px]"
           style="border: 0"
           allowfullscreen=""
           loading="lazy"
